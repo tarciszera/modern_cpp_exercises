0e75a9ba613c0c4306b3ac89faf97637c5cb19fd
Tarcis Becher <tarcis.4b@gmail.com>
2021-04-25 19:45:01 -0300

    [C++-0] Initial commit.

---
+++ chapter_1/sum_3_5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <boost/math/quaternion.hpp>
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    boost::math::quaternion<float> q0, q2;
+    cout << "Hello World!" << q0;
+}
