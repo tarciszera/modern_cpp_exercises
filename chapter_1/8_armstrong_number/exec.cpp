b27c3182ad9b6563e49e39b63352e9fbf271c012
Tarcis Becher <tarcis.4b@gmail.com>
2021-04-27 22:13:37 -0300

    [C++-5] Add ch1, ex 8.

--- chapter_1/8_armstrong_number/exec.cpp
+++ chapter_1/8_armstrong_number/exec.cpp
@@ -5,14 +5,20 @@
 
 #include <functions.h>
 
+#define POWER_3(a)  (a * a * a)
+
 int main(int argc, char** argv) {
-    unsigned int in;
-    for (unsigned int i = 1; i < 1000000; i++) {
-        unsigned int j = abundance(i);
-        if (i == abundance(j)) {
-            std::stringstream out;
-            out << i << " " << j << " are amicables";
-            print_(out.str());
+    std::string out = "The 3 digits armstrong numbers are:";
+    print_(out);
+    unsigned int c, d, u;
+    for (unsigned int i = 100; i < 1000; i++) {
+        u = (i % 10);
+        d = (i % 100) / 10;
+        c = (i % 1000) / 100;
+        if (i == (POWER_3(c) + POWER_3(d) + POWER_3(u))) {
+            std::stringstream armstrong;
+            armstrong << i;
+            print_(armstrong.str());
         }
     }
     return 0;
